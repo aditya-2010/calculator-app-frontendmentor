66963bc6b3579dc60e4334b590ab736aa56abca6
Aditya Nakadi <nakadiaditya@gmail.com>
2023-07-13 23:38:55 +0530

    feat: theme switching

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { evaluate } from "mathjs";
+import ThemeSwitcher from "./ThemeSwitcher";
+
+const operations = ["+", "-", "/", "*", "."];
 
 function App() {
+  const [current, setCurrent] = useState("");
+
+  function handleClick(value) {
+    if (
+      (operations.includes(value) && current === "") ||
+      (operations.includes(value) && operations.includes(current.slice(-1)))
+    )
+      return;
+
+    if (value === "=") {
+      setCurrent((curr) => evaluate(curr).toString());
+      return;
+    }
+    setCurrent(current + value);
+  }
+
+  function handleDelete() {
+    setCurrent((curr) => curr.slice(0, -1));
+  }
+
+  function handleReset() {
+    setCurrent("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="calculator">
+      <header className="header">
+        <div className="title">calc</div>
+        <ThemeSwitcher />
       </header>
+      <div className="output">
+        <div className="current-operand">{current || "0"}</div>
+      </div>
+      <div className="calc-grid">
+        <button onClick={() => handleClick("7")}>7</button>
+        <button onClick={() => handleClick("8")}>8</button>
+        <button onClick={() => handleClick("9")}>9</button>
+        <button onClick={handleDelete} className="btn-dark">
+          DEL
+        </button>
+        <button onClick={() => handleClick("4")}>4</button>
+        <button onClick={() => handleClick("5")}>5</button>
+        <button onClick={() => handleClick("6")}>6</button>
+        <button onClick={() => handleClick("+")}>+</button>
+        <button onClick={() => handleClick("1")}>1</button>
+        <button onClick={() => handleClick("2")}>2</button>
+        <button onClick={() => handleClick("3")}>3</button>
+        <button onClick={() => handleClick("-")}>-</button>
+        <button onClick={() => handleClick(".")}>.</button>
+        <button onClick={() => handleClick("0")}>0</button>
+        <button onClick={() => handleClick("/")}>/</button>
+        <button onClick={() => handleClick("*")}>x</button>
+        <button onClick={handleReset} className="span-2 btn-dark">
+          RESET
+        </button>
+        <button onClick={() => handleClick("=")} className="span-2 btn-equal">
+          =
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/ThemeSwitcher.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+export default function ThemeSwitcher() {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(
+    function () {
+      document.documentElement.setAttribute("color-scheme", theme);
+    },
+    [theme]
+  );
+
+  function handleChange(e) {
+    setTheme(e.target.value);
+  }
+
+  return (
+    <div className="theme-switcher">
+      <span className="theme">THEME</span>
+      <div class="tw-toggle">
+        <label htmlFor="dark">1</label>
+        <input
+          checked={theme === "dark"}
+          type="radio"
+          name="toggle"
+          value="dark"
+          id="dark"
+          onChange={(e) => handleChange(e)}
+        />
+
+        <label htmlFor="light">2</label>
+        <input
+          checked={theme === "light"}
+          type="radio"
+          name="toggle"
+          value="light"
+          id="light"
+          onChange={(e) => handleChange(e)}
+        />
+
+        <label htmlFor="violet">3</label>
+        <input
+          checked={theme === "violet"}
+          type="radio"
+          name="toggle"
+          value="violet"
+          id="violet"
+          onChange={(e) => handleChange(e)}
+        />
+
+        <span></span>
+      </div>
+    </div>
+  );
+}
